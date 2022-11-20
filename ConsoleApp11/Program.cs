8553ef8ae6ab4353fe8dae9d3add706693d7701a
unknown <perviz.elizade.2002@mail.ru>
2022-11-21 02:20:35 +0400

    commit

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Parviz","Alizada");
+            Console.WriteLine("********* Student 1 *********");
+
+            Console.WriteLine(student1.Name+" "+student1.SurName);
+
+            Console.WriteLine("********* Student 2 *********");
+            
+            Student student2 = new Student("Vefa", "Farzaliyeva", 683, 87.5, true);
+
+            Console.WriteLine(student2.Name + " " + student2.SurName + " "+student2.Group+" "+student2.Point+" "+student2.Gruated()+" "+student2.JoinExam());
+
+        }
+    }
+}
---
+++ ConsoleApp11/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    class Student
+    {
+       public string Name;
+       public string SurName;
+       public double Group;
+       public double Point;
+       public bool IsGruated;
+
+        public Student()
+        {
+
+        }
+        public Student(string name,string surname)
+        {
+            Name=name;
+            SurName = surname;
+        }
+        public Student(string name,string surname,double group,double point,bool isgruated)
+        {
+            Name = name;
+            SurName = surname;
+            Group = group;
+            Point = point;
+            IsGruated = isgruated;
+        }
+
+        public string Gruated()
+        {
+            if (IsGruated)
+            {
+                return "Telebe Mezun olub";
+            }
+            else
+            {
+                return "Telebe Mezun olmuyub";
+            }
+        }
+
+        public string JoinExam()
+        {
+            if (Point>80)
+            {
+                return "Telebe imtahana daxil ola biler";
+            }
+            return "Telebe Imtahana daxil ola bilmez";
+        }
+    }
+}
