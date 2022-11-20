52b5773a64d710f3c10c1395506cc0d63f23b425
unknown <perviz.elizade.2002@mail.ru>
2022-11-20 22:33:40 +0400

    first commit

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp10
+{
+	class Program
+	{
+		#region Task1
+		//static void Main(string[] args)
+		//{
+		//	string text = Console.ReadLine();
+		//	Console.WriteLine(CapitalizeWords(text));
+
+		//}
+		//public static string CapitalizeWords(string value)
+		//{
+		//	if (value == null)
+		//		throw new ArgumentNullException("value");
+		//	if (value.Length == 0)
+		//		return value;
+		//	StringBuilder result = new StringBuilder(value);
+		//	result[0] = char.ToUpper(result[0]);
+		//	for (int i = 1; i < result.Length; ++i)
+		//	{
+		//		if (char.IsWhiteSpace(result[i - 1]))
+		//			result[i] = char.ToUpper(result[i]);
+		//	}
+
+		//	return result.ToString();
+		//}
+		#endregion
+
+	}
+}
+
+
+
